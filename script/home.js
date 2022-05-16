481077372043b8250e601a5b8c8fd1897dfe7d7d
Julia Lendzion <juli47g3@stud.kea.dk>
2022-05-16 23:21:37 +0200

    circle spinning

--- script/home.js
+++ script/home.js
@@ -1,8 +1,27 @@
+// gsap.to("#circular-icon", {
+//   rotate: 360,
+//   duration: 4,
+//   toggleActions: "play none none reverse",
+//   scrollTrigger: { trigger: "#circular-icon", start: "100px 200px", end: "200px", markers: true },
+// });
+
 console.log("heeeej");
 
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.set("#circular-icon", { xPercent: -50 });
 
-// let smoother = ScrollSmoother.create({
-//   wrapper: "#smooth-wrapper",
-//   content: "#smooth-content",
-// });
+var rotate = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: "#circular-icon",
+      pin: true,
+      scrub: 0.2,
+      start: "bottom bottom",
+      end: "+=10000",
+      markers: true,
+    },
+  })
+  .to("#circular-icon", {
+    rotation: 360 * 5,
+    duration: 1,
+    ease: "none",
+  });

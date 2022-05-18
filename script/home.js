e89a81566626e614ba194d7529ff1b9bb59716e9
Julia Lendzion <juli47g3@stud.kea.dk>
2022-05-18 21:34:03 +0200

    home page animations and 2 new sections

--- script/home.js
+++ script/home.js
@@ -1,27 +1,62 @@
-// gsap.to("#circular-icon", {
-//   rotate: 360,
-//   duration: 4,
-//   toggleActions: "play none none reverse",
-//   scrollTrigger: { trigger: "#circular-icon", start: "100px 200px", end: "200px", markers: true },
-// });
+gsap.set("#circular-icon", { xPercent: 0 });
 
-console.log("heeeej");
-
-gsap.set("#circular-icon", { xPercent: -50 });
-
-var rotate = gsap
+const rotate = gsap
   .timeline({
     scrollTrigger: {
       trigger: "#circular-icon",
-      pin: true,
       scrub: 0.2,
-      start: "bottom bottom",
-      end: "+=10000",
+      start: 0,
+      end: 1000,
       markers: true,
     },
   })
   .to("#circular-icon", {
-    rotation: 360 * 5,
+    rotation: 360 * 1,
     duration: 1,
     ease: "none",
   });
+
+// const pin = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: "#circular-icon",
+//     pin: true,
+//     start: "bottom bottom",
+//     markers: true,
+//   },
+// });
+
+/// images - ease in
+// const delSections = document.querySelectorAll(".delayed-section");
+
+// delSections.forEach((section) => {
+//   const containerAnim = gsap.to(section.querySelector(".innerContainer"), {
+//     y: "100vh",
+//     ease: "none",
+//   });
+
+//   const imageAnim = gsap.to(section.querySelector("img"), {
+//     y: "-100vh",
+//     ease: "none",
+//     paused: true,
+//   });
+
+//   const scrub = gsap.to(imageAnim, {
+//     progress: 1,
+//     paused: true,
+//     ease: "power3",
+//     duration: parseFloat(section.dataset.scrub) || 0.1,
+//     overwrite: true,
+//   });
+
+//   ScrollTrigger.create({
+//     animation: containerAnim,
+//     scrub: true,
+//     trigger: section,
+//     start: "top bottom",
+//     end: "bottom top",
+//     onUpdate: (self) => {
+//       scrub.vars.progress = self.progress;
+//       scrub.invalidate().restart();
+//     },
+//   });
+// });

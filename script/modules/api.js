1c57159bd662340c520d547c2479093fa978733d
Julia Lendzion <juli47g3@stud.kea.dk>
2022-05-20 15:16:48 +0200

    product scroll

--- script/modules/api.js
+++ script/modules/api.js
@@ -1,28 +1,14 @@
 let url = "https://kea2021-907c.restdb.io/rest/puuf";
-
-/*API key*/
 const options = {
   headers: {
     "x-apikey": "602e264f5ad3610fb5bb6267",
   },
 };
 
-function getData() {
-  fetch(url, options)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-
-    .then((data) => {
-      // console.log(data);
-      handleData(data);
-    })
-    .catch((e) => {
-      console.error("an error occured:", e.message);
-    });
+async function get_products() {
+  const response = await fetch(url, options);
+  const jsonData = await response.json();
+  return jsonData;
 }
 
-export { getData };
+export { get_products };

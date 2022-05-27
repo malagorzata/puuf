83543fc4503d7e7f4bdfdd7a93fcc2db0aaa5c7c
Gosia <gosia.warwaszynska@gmail.com>
2022-05-27 17:01:30 +0200

    submit button works

--- productview.js
+++ productview.js
@@ -41,3 +41,33 @@ function handleProducts(products) {
   document.querySelector(".second alt").textContent = products.name;
   document.querySelector(".third alt").textContent = products.name;
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(form.elements.email.value);
+
+  const payload = {
+    email: form.elements.email.value,
+  };
+
+  document.querySelector("input[type=submit]").disabled = true;
+
+  fetch("https://kea2021-907c.restdb.io/rest/puuf-newsletter  ", {
+    method: "POST",
+    headers: {
+      "x-apikey": "602e264f5ad3610fb5bb6267",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      console.log(response);
+      document.querySelector("input[type=submit]").disabled = false;
+      form.elements.email.value = "";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});

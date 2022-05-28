456222125d6a65c7b681cf912f6fe807de308c01
Julia Lendzion <juli47g3@stud.kea.dk>
2022-05-28 12:24:05 +0200

    form

--- script/form/form.js
+++ script/form/form.js
@@ -3,6 +3,21 @@ console.log("hejka to form.js");
 const form = document.querySelector("#contact-form");
 console.log(form.elements);
 
+form.addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("#contact-form");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
 document.querySelector("#button-submit").addEventListener("click", checkValidity);
 
 function checkValidity() {

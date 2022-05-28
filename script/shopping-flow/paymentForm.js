d9e9bf70a5bab0dd2e782ca96a0c565c4386f0bd
Gosia <gosia.warwaszynska@gmail.com>
2022-05-28 18:20:44 +0200

    trying to handle form

--- script/shopping-flow/paymentForm.js
+++ script/shopping-flow/paymentForm.js
@@ -1,12 +1,12 @@
 console.log("hejka to form.js");
 
-const form = document.querySelector(".paymentForm");
+const form = document.querySelector("#payment-form");
 // console.log(form.elements);
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  let myForm = document.getElementsByClassName(".paymentForm");
-  let formData = new FormData(contactForm);
+  let myForm = document.getElementById("payment-form");
+  let formData = new FormData(paymentForm);
   document.querySelector("button[class=place-order]").disabled = true;
   fetch("/", {
     method: "POST",
@@ -15,20 +15,13 @@ const handleSubmit = (e) => {
   })
     .then(() => {
       console.log("form submited sucesfully");
-      document.querySelector("button[id=button-submit]").disabled = false;
-      document.querySelector("input[id=name]").value = "";
-      document.querySelector("input[id=email]").value = "";
-      document.querySelector("textarea[id=message]").value = "";
-      document.querySelector("input[type=checkbox]").checked = false;
-      document.querySelector("#confirmation-text").classList.remove("hidden");
-      setTimeout(removeMessege, 7000);
     })
     .catch((error) => alert(error));
 };
 
 form.addEventListener("submit", handleSubmit);
 
-function removeMessege() {
-  document.querySelector("#confirmation-text").classList.add("hidden");
-}
+// function removeMessege() {
+//   document.querySelector("#confirmation-text").classList.add("hidden");
+// }
 // do

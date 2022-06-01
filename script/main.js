48a7b9e7ec88452a569d6a8a32e6f031b51bcdcf
Gosia <gosia.warwaszynska@gmail.com>
2022-06-01 15:14:56 +0200

    menu social media added

--- script/main.js
+++ script/main.js
@@ -48,31 +48,3 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
-// const text = document.querySelector(".title");
-// const fullText = text.textContent;
-// const splitText = fullText.split("");
-// text.textContent = "";
-
-// for (let i = 0; i < splitText.length; i++) {
-//   text.innerHTML += "<span>" + splitText[i] + "</span>";
-// }
-
-// let char = 0;
-// let timer = setInterval(onTick, 50);
-
-// function onTick() {
-//   const span = text.querySelectorAll("span")[char];
-
-//   span.classList.add("fade");
-//   char++;
-
-//   if (char === splitText.length) {
-//     complete();
-//     return;
-//   }
-// }
-
-// function complete() {
-//   clearInterval(timer);
-//   timer = null;
-// }
